0cfba67dd278c7cb26ba8091a8f8d57e1854517b
VmayaV <maja.vladimirova@gmail.com>
2021-12-12 21:57:15 +0100

    fonts, resources, design updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,21 +14,57 @@ module.exports = {
       colors: {
         'dark-blue': '#123B4F',
         'dark-gray': '#3C3C3B',
-        'light-blue': '#95B8D7'
+        'light-blue': '#95B8D7',
+        'light-gray': '#E7E1DD'
       },
       fontFamily: {
-
+        'veener-two': ['Veneer Two, sens-serif'],
+        'gotham': ['Gotham, sens-serif']
+      },
+      fontSize: {
+        xs: ['0.75rem', { lineHeight: '0.875rem' }],
+        sm: {
+          '20lh': ['0.875rem', { lineHeight: '1.25rem' }],
+          '32lh': ['0.875rem', { lineHeight: '2rem' }],
+        },
+        md: {
+          18: ['1.125rem', { lineHeight: '1.75rem' }],
+          20: ['1.25rem', { lineHeight: '1.75rem' }],
+        },
+        lg: ['32px', {
+          lineHeight: '38px',
+        }],
+        xl: ['50px', {
+          lineHeight: '55px',
+        }]
       },
       spacing: {
         '12.5': '3.25rem',
         '16.5': '4rem',
       }
-
-
     }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        '@font-face': {
+          fontFamily: 'Veneer Two',
+          fontWheight: '400',
+          src: 'url(../assets/fonts/VeneerTwo.ttf)'
+        }
+      })
+    },
+    function ({ addBase }) {
+      addBase({
+        '@font-face': {
+          fontFamily: 'Gotham',
+          fontWheight: '400',
+          src: 'url(../assets/fonts/GothamLight.ttf)'
+        }
+      })
+    }
+  ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,10 @@ module.exports = {
                 type: "asset/resource",
             },
             {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
                 use: [
